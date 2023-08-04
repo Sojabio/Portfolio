328779c99e7ccac43bae7c1a290604403977883e
soja <solene.jabaud@gmail.com>
2023-08-04 13:32:39 +0200

    fix modes

--- src/components/Buttons/toggle.jsx
+++ src/components/Buttons/toggle.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import { DarkModeContext } from "../../contexts/DarkModeProvider";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMoon } from '@fortawesome/free-solid-svg-icons'; // Import faMoon icon
+import { faMoon } from '@fortawesome/free-solid-svg-icons';
 import { faSun } from '@fortawesome/free-solid-svg-icons';
 export default function ToggleButton() {
   const { darkMode, toggleDarkMode } = useContext(DarkModeContext);
@@ -10,7 +10,7 @@ export default function ToggleButton() {
     <>
       <div className="nav-toggle" onClick={toggleDarkMode}>
         {darkMode ? (
-          <FontAwesomeIcon icon={faSun} alt="switch to light mode" /> // Use faMoon icon
+          <FontAwesomeIcon icon={faSun} alt="switch to light mode" /> 
         ) : (
           <FontAwesomeIcon icon={faMoon} alt="switch to dark mode" />
         )}
