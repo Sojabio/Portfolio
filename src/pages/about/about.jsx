5ceb8c3dfd35868b2a34ba068e1ddd3ec6689f89
soja <solene.jabaud@gmail.com>
2023-08-05 17:11:15 +0200

    add about

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -1,11 +1,38 @@
 import './about.css'
+import {useState} from 'react';
 
 const About = () => {
+  const [concernsContent, setConcernsContent] = useState('[...]')
+
+  const handleClickConcerns = () => {
+    concernsContent === '[...]' ? setConcernsContent(subjects) : setConcernsContent('[...]')
+  }
+
+  const subjects = () => {
+    return (
+      <div>
+        les enjeux auxquels je suis sensible:
+        <ul>
+          <li>l'écologie</li>
+          <li>l'éducation</li>
+          <li>l'inclusivité</li>
+          <li>l'accessibilité</li>
+        </ul>
+      </div>
+    )
+  }
+
   return (
     <div className="about">
-      <p>J'aime <span>résoudre</span> des problèmes et <span>créer</span> avec mes mains ou mon esprit. </p>
-      <p>Après avoir appris le <span>chinois</span> et maîtrisé le travail du <span>bois</span>, j'ai choisi de réconcilier mes centres d'intérêt en concevant des objets virtuels à l'aide de <span>langages</span> plus abstraits.</p>
-      <span>J'ai à coeur de mettre mes compétences au service de projets stimulants et porteurs de sens.</span>
+      <div>
+    <p>J'aime <span>résoudre</span> des problèmes et <span>créer</span> avec mes mains ou mon esprit. </p>
+    <p>Après avoir appris le <span>chinois</span> et maîtrisé le travail du <span>bois</span>, j'ai choisi de réconcilier mes centres d'intérêt en concevant des objets virtuels à l'aide de <span>langages</span> plus abstraits.</p>
+    <span>J'ai à cœur de mettre mes compétences au service de projets stimulants et porteurs de sens.</span>
+      </div>
+      <div className="concerns"
+        onClick={handleClickConcerns}>
+        <p>{concernsContent}</p>
+      </div>
     </div>
   )
 }
